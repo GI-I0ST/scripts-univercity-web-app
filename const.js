e3de035c210930a7fb313838aad2e8c0feb7aa45
Bodia <bodikandrusenko@gmail.com>
2022-04-01 21:12:35 +0300

    final heroku db config

--- const.js
+++ const.js
@@ -1,17 +1,7 @@
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:12345678@localhost:5432/music';
-const DB_NAME = process.env.DB_NAME || 'music';
-const DB_PASSWORD = process.env.DB_PASSWORD || '12345678';
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || '5432';
-const DB_USER = process.env.DB_USER || 'postgres';
 
 console.log("process.env.DB_USER", process.env.DB_USER);
 
 module.exports = {
-    DB_NAME,
-    DB_PASSWORD,
-    DB_HOST,
-    DB_PORT,
-    DB_USER,
     DATABASE_URL
 };
--- db/db.js
+++ db/db.js
@@ -3,30 +3,11 @@ const Sequelize = require('sequelize');
 
 console.log("env log", process.env.DB_USER);
 console.log("constants log", constants.DB_USER);
-// Or you can simply use a connection uri
-//module.exports = new Sequelize(constants.DB);
-/*
-module.exports = new Sequelize({
-    database: constants.DB_NAME,
-    username: constants.DB_USER,
-    password: constants.DB_PASSWORD,
-    host: constants.DB_HOST,
-    port: constants.DB_PORT,
-    dialect: "postgres",
-    ssl: true,
-    dialectOptions: {
-        ssl: {
-            require: true, // This will help you. But you will see nwe error
-            rejectUnauthorized: false // This line will fix new error
-        }
-    },
-});
-*/
 
 sequelize = new Sequelize(constants.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    logging:  true, //false,
+    // logging:  true, //false,
     dialectOptions: {
         ssl: {
             require: true, // This will help you. But you will see nwe error
