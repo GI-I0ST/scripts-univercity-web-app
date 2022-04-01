0cc34efdb8338d41501ceab2e28f324ba3a341be
Bodia <bodikandrusenko@gmail.com>
2022-04-01 20:35:57 +0300

    log env constant

--- const.js
+++ const.js
@@ -4,6 +4,8 @@ const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || '5432';
 const DB_USER = process.env.DB_USER || 'postgres';
 
+console.log("process.env.DB_USER", process.env.DB_USER);
+
 module.exports = {
     DB_NAME,
     DB_PASSWORD,
